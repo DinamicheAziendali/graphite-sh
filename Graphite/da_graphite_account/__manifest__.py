bde8033e612a915343ef7a16fbe017949bd4607f
Borruso <gborruso@dinamicheaziendali.it>
2023-02-27 15:14:30 +0100

    [ADD] da_graphite_account: aggiunto modulo che skippa controllo XML per fatture

---
+++ Graphite/da_graphite_account/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2018-Today:
+# Dinamiche Aziendali Srl (<http://www.dinamicheaziendali.it/>)
+# @author: Giuseppe Borruso <gborruso@dinamicheaziendali.it>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Graphite - Account - Customizations",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Customizations/Graphite",
+    "author": "Dinamiche Aziendali srl",
+    "website": "https://www.dinamicheaziendali.it",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_it_fatturapa_out",
+    ],
+    "data": [],
+    "installable": True,
+}
